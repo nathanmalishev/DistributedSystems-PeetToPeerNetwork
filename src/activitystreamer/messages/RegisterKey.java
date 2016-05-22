3df3c17a36e559e0ad0afcd8914d0206ae743339
rokane <rokane@student.unimelb.edu.au>
2016-05-22 18:16:20 +1000

    Added functionality to read RegisterKey message and save the publicKey/hostname:port pair in local storage

--- src/activitystreamer/messages/RegisterKey.java
+++ src/activitystreamer/messages/RegisterKey.java
@@ -2,11 +2,21 @@ package activitystreamer.messages;
 
 public class RegisterKey extends JsonMessage {
 
-	private String serverId;	
+	private String serverId;
+	private String publicKey;
 	
-	public RegisterKey(String id){
+	public RegisterKey(String id, String pk){
 		
 		this.command = "REGISTER_KEY";
 		this.serverId = id;
+		this.publicKey = pk;
+	}
+	
+	public String getServerId(){
+		return serverId;
+	}
+	
+	public String getPublicKey(){
+		return publicKey;
 	}
 }
